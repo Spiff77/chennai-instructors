d3210a8495be228f3fd2b824bea834887df8fc74
Thomas Lhomme <git@treelevel.io>
2026-08-20 07:46:29 +0530

    readme

---
+++ resources/lab13-fixes/13-demo.sql
@@ -0,0 +1,5 @@
+INSERT INTO instruments (ticker, name) VALUES
+    ('VOD.L','Vodafone Group PLC'), ('BARC.L','Barclays PLC'),
+    ('ULVR.L','Unilever PLC'),      ('AAPL','Apple Inc'),
+    ('GILT10','UK 10-Year Gilt'),   ('CORPB1','Sterling Corporate Bond Fund'),
+    ('GLBEQ1','Global Equity Index Fund'), ('CASHGBP','Cash (GBP)');
